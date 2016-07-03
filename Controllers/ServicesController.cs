8f4959d59ccf809d3e263b95618ac99624160d95
Percy Mukwenya <pfmukwenya@gmail.com>
2016-07-03 17:51:12 +0200

    controllers

---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiaphonV2.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Print()
+        {
+            ViewBag.Message = "This is the Print Service Page.";
+
+            return View();
+        }
+
+        public ActionResult Branding()
+        {
+            ViewBag.Message = "This is the Branding & Design Service Page.";
+
+            return View();
+        }
+        
+        public ActionResult Digital()
+        {
+            ViewBag.Message = "This is the Digital Solutions Service Page.";
+
+            return View();
+        }
+    }
+}
